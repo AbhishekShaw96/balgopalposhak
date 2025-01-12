11437a6fc4b5a49ab913b19b6cd814ab6b39a0e4
aabhishekk7 <abhishekshaww7@gmail.com>
2025-01-12 15:26:42 +0530

    Initial commit

---
+++ balgopalposhak/src/main/java/com/balgopalposhak/balgopalposhak/BalgopalposhakApplication.java
@@ -0,0 +1,13 @@
+package com.balgopalposhak.balgopalposhak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalgopalposhakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalgopalposhakApplication.class, args);
+	}
+
+}
