575505a78cc9cf0e04681ec07203eb83b23797b8
aabhishekk7 <abhishekshaww7@gmail.com>
2025-01-12 16:39:32 +0530

    Dev Commit

--- user-service/src/main/java/com/balgopalposhak/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/balgopalposhak/user_service/UserServiceApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class UserServiceApplication {
 
+	//testing
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
